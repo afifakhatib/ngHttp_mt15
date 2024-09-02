bade6ccf376123ef924e7ceaeed0d06cb65256c8
afifakhatib <khatibadiba12@gmail.com>
2024-09-02 12:57:28 +0530

    final commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   BASE_URL : 'https://b12-project-database-default-rtdb.asia-southeast1.firebasedatabase.app',
 };
 
